26e64a8b30ab1d6c5a03a5ae4637fd78049a66d5
shanth kuamer <shanth923@gmail.com>
2020-01-20 18:46:20 +0530

    Initial Commit

---
+++ structs,overloading/Structs.swift
@@ -0,0 +1,39 @@
+//
+//  Structs.swift
+//  structs,overloading
+//
+//  Created by R Shantha Kumar on 8/12/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class Structs {
+    
+    var engMarks:UInt8?
+    var hindMarks:UInt8?
+    var telMarks:UInt8?
+    var mathMarks:UInt8?
+    var sciMarks:UInt8?
+    var socMarks:UInt8?
+    var passmarks:UInt8=35
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
---
+++ structs,overloading/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  structs,overloading
+//
+//  Created by R Shantha Kumar on 8/12/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
